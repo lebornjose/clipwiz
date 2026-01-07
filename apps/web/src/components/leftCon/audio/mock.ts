f1ea7925839fd913af6ce3dff13fb534bad2ddfb
黄兴 <huangxing@huangxingdeMacBook-Pro.local>
2026-01-07 20:41:59 +0800

    feat: 贴纸

--- apps/web/src/components/leftCon/audio/mock.ts
+++ apps/web/src/components/leftCon/audio/mock.ts
@@ -3,17 +3,98 @@
 export const audioList = [
   {
     id: 1,
-    name: '音频1',
+    name: '鞋子磨擦地音效一下',
+    duration: '00:00',
     url: 'https://mogic-effect-test.oss-cn-hangzhou.aliyuncs.com/batch_fusion_demo/material/ou_bbf7de6f3a9c8e7545112c4f3dd267ab/%E9%9E%8B%E5%AD%90%E7%A3%A8%E6%93%A6%E5%9C%B0%E9%9F%B3%E6%95%88%E4%B8%80%E4%B8%8B.MP3'
   },
   {
     id: 2,
-    name: '手摸鞋子音效',
+    name: '鞋子磨擦地音效X2下',
+    duration: '00:00',
     url: 'https://mogic-effect-test.oss-cn-hangzhou.aliyuncs.com/batch_fusion_demo/material/ou_bbf7de6f3a9c8e7545112c4f3dd267ab/%E6%89%8B%E6%91%B8%E9%9E%8B%E5%AD%90%E9%9F%B3%E6%95%88-%E5%90%88%E9%80%82%E7%9A%84%E5%9C%B0%E6%96%B9%E7%94%A8.MP3'
   },
   {
     id: 3,
-    name: '礼炮的音效',
+    name: '手摸鞋子音效',
+    duration: '00:01',
     url: 'https://mogic-effect-test.oss-cn-hangzhou.aliyuncs.com/batch_fusion_demo/material/ou_e5ef2237b42b51f5edd7e197c78acd29/%E7%A4%BC%E7%82%AE%E7%9A%84%E9%9F%B3%E6%95%88.MP3'
   },
+  {
+    id: 4,
+    name: '秋冬款鞋子音效',
+    duration: '00:00',
+    url: 'https://mogic-effect-test.oss-cn-hangzhou.aliyuncs.com/batch_fusion_demo/material/ou_bbf7de6f3a9c8e7545112c4f3dd267ab/%E9%9E%8B%E5%AD%90%E7%A3%A8%E6%93%A6%E5%9C%B0%E9%9F%B3%E6%95%88%E4%B8%80%E4%B8%8B.MP3'
+  },
+  {
+    id: 5,
+    name: '【动作】跑冰砰水',
+    duration: '00:01',
+    url: 'https://mogic-effect-test.oss-cn-hangzhou.aliyuncs.com/batch_fusion_demo/material/ou_bbf7de6f3a9c8e7545112c4f3dd267ab/%E6%89%8B%E6%91%B8%E9%9E%8B%E5%AD%90%E9%9F%B3%E6%95%88-%E5%90%88%E9%80%82%E7%9A%84%E5%9C%B0%E6%96%B9%E7%94%A8.MP3'
+  },
+  {
+    id: 6,
+    name: '礼炮鞋底声音',
+    duration: '00:01',
+    url: 'https://mogic-effect-test.oss-cn-hangzhou.aliyuncs.com/batch_fusion_demo/material/ou_e5ef2237b42b51f5edd7e197c78acd29/%E7%A4%BC%E7%82%AE%E7%9A%84%E9%9F%B3%E6%95%88.MP3'
+  },
+  {
+    id: 7,
+    name: '容题正确提示',
+    duration: '00:01',
+    url: 'https://mogic-effect-test.oss-cn-hangzhou.aliyuncs.com/batch_fusion_demo/material/ou_bbf7de6f3a9c8e7545112c4f3dd267ab/%E9%9E%8B%E5%AD%90%E7%A3%A8%E6%93%A6%E5%9C%B0%E9%9F%B3%E6%95%88%E4%B8%80%E4%B8%8B.MP3'
+  },
+  {
+    id: 8,
+    name: '喵影效呼声',
+    duration: '00:02',
+    url: 'https://mogic-effect-test.oss-cn-hangzhou.aliyuncs.com/batch_fusion_demo/material/ou_bbf7de6f3a9c8e7545112c4f3dd267ab/%E6%89%8B%E6%91%B8%E9%9E%8B%E5%AD%90%E9%9F%B3%E6%95%88-%E5%90%88%E9%80%82%E7%9A%84%E5%9C%B0%E6%96%B9%E7%94%A8.MP3'
+  },
+  {
+    id: 9,
+    name: '胶片滚动声',
+    duration: '00:01',
+    url: 'https://mogic-effect-test.oss-cn-hangzhou.aliyuncs.com/batch_fusion_demo/material/ou_e5ef2237b42b51f5edd7e197c78acd29/%E7%A4%BC%E7%82%AE%E7%9A%84%E9%9F%B3%E6%95%88.MP3'
+  },
+  {
+    id: 10,
+    name: '扣留必是一下音效',
+    duration: '00:00',
+    url: 'https://mogic-effect-test.oss-cn-hangzhou.aliyuncs.com/batch_fusion_demo/material/ou_bbf7de6f3a9c8e7545112c4f3dd267ab/%E9%9E%8B%E5%AD%90%E7%A3%A8%E6%93%A6%E5%9C%B0%E9%9F%B3%E6%95%88%E4%B8%80%E4%B8%8B.MP3'
+  },
+  {
+    id: 11,
+    name: '苏水原声',
+    duration: '00:02',
+    url: 'https://mogic-effect-test.oss-cn-hangzhou.aliyuncs.com/batch_fusion_demo/material/ou_bbf7de6f3a9c8e7545112c4f3dd267ab/%E6%89%8B%E6%91%B8%E9%9E%8B%E5%AD%90%E9%9F%B3%E6%95%88-%E5%90%88%E9%80%82%E7%9A%84%E5%9C%B0%E6%96%B9%E7%94%A8.MP3'
+  },
+  {
+    id: 12,
+    name: '鞋子磨擦声',
+    duration: '00:00',
+    url: 'https://mogic-effect-test.oss-cn-hangzhou.aliyuncs.com/batch_fusion_demo/material/ou_e5ef2237b42b51f5edd7e197c78acd29/%E7%A4%BC%E7%82%AE%E7%9A%84%E9%9F%B3%E6%95%88.MP3'
+  },
+  {
+    id: 13,
+    name: '扣留必是',
+    duration: '00:02',
+    url: 'https://mogic-effect-test.oss-cn-hangzhou.aliyuncs.com/batch_fusion_demo/material/ou_bbf7de6f3a9c8e7545112c4f3dd267ab/%E9%9E%8B%E5%AD%90%E7%A3%A8%E6%93%A6%E5%9C%B0%E9%9F%B3%E6%95%88%E4%B8%80%E4%B8%8B.MP3'
+  },
+  {
+    id: 14,
+    name: '叮2',
+    duration: '00:01',
+    url: 'https://mogic-effect-test.oss-cn-hangzhou.aliyuncs.com/batch_fusion_demo/material/ou_bbf7de6f3a9c8e7545112c4f3dd267ab/%E6%89%8B%E6%91%B8%E9%9E%8B%E5%AD%90%E9%9F%B3%E6%95%88-%E5%90%88%E9%80%82%E7%9A%84%E5%9C%B0%E6%96%B9%E7%94%A8.MP3'
+  },
+  {
+    id: 15,
+    name: '滑稽综艺音效 呦啊',
+    duration: '00:01',
+    url: 'https://mogic-effect-test.oss-cn-hangzhou.aliyuncs.com/batch_fusion_demo/material/ou_e5ef2237b42b51f5edd7e197c78acd29/%E7%A4%BC%E7%82%AE%E7%9A%84%E9%9F%B3%E6%95%88.MP3'
+  },
+  {
+    id: 16,
+    name: '卡通叮叮声',
+    duration: '00:01',
+    url: 'https://mogic-effect-test.oss-cn-hangzhou.aliyuncs.com/batch_fusion_demo/material/ou_bbf7de6f3a9c8e7545112c4f3dd267ab/%E9%9E%8B%E5%AD%90%E7%A3%A8%E6%93%A6%E5%9C%B0%E9%9F%B3%E6%95%88%E4%B8%80%E4%B8%8B.MP3'
+  }
 ]
--- apps/web/src/components/leftCon/index.tsx
+++ apps/web/src/components/leftCon/index.tsx
@@ -5,14 +5,15 @@ import {
  } from '@ant-design/icons'
 import MaterialList from './material'
 import AudioList from './audio'
+import PhotoList from './photo'
 import './index.less'
 
 const LeftCon = () => {
   const items = [
       {key: '1', icon: <VideoCameraOutlined />, label: '素材', children: <MaterialList />},
       {key: '2', icon: <AudioOutlined />, label: '音频', children: <AudioList />},
-      {key: '3', icon: <FileTextOutlined />, label: '文本', children: <div>图片</div>},
-      {key: '4', icon: <FileImageOutlined />, label: '贴纸', children: <div>文字</div>},
+      // {key: '3', icon: <FileTextOutlined />, label: '文本', children: <div>图片</div>},
+      {key: '4', icon: <FileImageOutlined />, label: '贴纸', children: <PhotoList />},
       {key: '5', icon: <FontSizeOutlined />, label: '特效', children: <div>形状</div>},
       {key: '6', icon: <SwapOutlined />, label: '转场', children: <div>其他</div>},
       {key: '7', icon: <FilterOutlined />, label: '滤镜', children: <div>其他</div>},
