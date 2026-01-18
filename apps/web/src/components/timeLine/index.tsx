3dd07dd7d77f23a3767fab88d21db8c837dc44fa
黄兴 <huangxing@huangxingdeMacBook-Pro.local>
2026-01-18 16:32:29 +0800

    feat: 1

--- apps/web/src/components/timeLine/index.tsx
+++ apps/web/src/components/timeLine/index.tsx
@@ -2,6 +2,7 @@
 import { Timeline, TimelineRow, TimelineState } from '@xzdarcy/react-timeline-editor';
 import { VideoCameraOutlined, AudioOutlined } from '@ant-design/icons';
 import { MATERIAL_TYPE } from '@clipwiz/shared';
+import { convertTrackInfoToTimelineRow } from './convert';
 import './index.less';
 import { CustomTimelineAction } from './mock';
 import { mockData, mockEffect } from './mock';
@@ -10,7 +11,8 @@ import trackInfo from '../../mock'
 const TimeLine = () => {
   const timelineState = useRef<TimelineState | null>(null);
   const domRef = useRef<HTMLDivElement | null>(null);
-  const [editorData, setEditorData] = useState<TimelineRow[]>(mockData);
+  const listData = convertTrackInfoToTimelineRow(trackInfo);
+  const [editorData, setEditorData] = useState<TimelineRow[]>(listData);
 
 
   return (
@@ -48,11 +50,11 @@ const TimeLine = () => {
           setEditorData(data as CustomTimelineAction[]);
         }}
         getActionRender={(action, row) => {
-          if(action.effectId === 'effect0') {
+          if(action.effectId === MATERIAL_TYPE.VIDEO) {
             return (
               <div className='effect-item-video'>播放视频</div>
             )
-          } else if(action.effectId === 'effect1') {
+          } else if(action.effectId === MATERIAL_TYPE.BGM_AUDIO) {
             return (
               <div className='effect-item-audio'>播放音效</div>
             )
